6f40d615b06777fc24a2dbad295f8ff21845bcf3
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2023-10-11 17:36:32 -0500

    Add files via upload

---
+++ GUI/src/Controlador/Controller.java
@@ -0,0 +1,38 @@
+package Controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Modelo.Numero;
+import Vista.VentanaPrincipal;
+
+public class Controller implements ActionListener {
+private VentanaPrincipal vista;
+
+public Controller() {
+	vista = new VentanaPrincipal();
+}
+
+public void asignarOyentes() {
+	vista.getPanel().getBotonValidar().addActionListener(this);
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	String comando = e.getActionCommand();
+	
+if(comando.equals("VALIDAR")) {
+	String aux1 = vista.getPanel().getCnumero().getText();
+	int aux2 = Integer.parseInt(aux1);
+	Numero num = new Numero(aux2);
+	
+	if(num.esPar()) {
+		vista.getPanel().getEmostrar().setText("Numero ingresado es PAR");
+	}else {
+		vista.getPanel().getEmostrar().setText("Numero ingresado es IMPAR");
+
+	}
+}
+}
+
+}
