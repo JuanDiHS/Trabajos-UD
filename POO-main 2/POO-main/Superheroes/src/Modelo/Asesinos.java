1bef13134fb4384f5127965ed8e9ebfbf82014b5
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2024-03-13 23:31:12 -0500

    Delete POO-main 2/POO-main/Superheroes directory

--- POO-main 2/POO-main/Superheroes/src/Modelo/Asesinos.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-public interface Asesinos {
-String MatarATodos(int CantidadEnemigos, int CantidadTiempo);
-String MatarSigilosamente (int CantidadEnemigos, String MomentoDelDia);
-String SacarUnArma (int CantidadArmas, String TipoArma);
-String Boxear (int CantidadMetros, int CantidadEnemigos);
-
-}
--- POO-main 2/POO-main/Superheroes/src/Modelo/Estrategas.java
+++
@@ -1,8 +0,0 @@
-package Modelo;
-
-public interface Estrategas {
-String CrearPlan (int CantidadTiempo, String TipoPlan);
-String HacerCalculos (String TipoCalculo, int CantidadSegundos);
-String PlanearRetirada(String LugarRetirada, int CantidadHeroesQueSeRetiran);
-String LlamarAmigos (int CantidadAmigos);
-}
