b02786a3fa355883c932cd6f92d49a6efcd48613
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2024-05-27 13:51:46 -0500

    Add files via upload

---
+++ Workshop_4/functions.js
@@ -0,0 +1,30 @@
+async function callMessage() {
+    try {
+        const response = await fetch('http://localhost/hello_ud');
+        const data = await response.text();
+        document.getElementById('result').textContent = data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function callTable() {
+    //function`s name was changed because in the html the name did not correspond 
+    try {
+        const response = await fetch('http://localhost/products');
+        const data = await response.json();
+        
+        let table = '<table>';
+        table += '<tr><th>ID</th><th>Name</th><th>Description</th></tr>';
+        
+        data.forEach(item => {
+            table += `<tr><td>${item.id}</td><td>${item.name}</td><td>${item.description}</td></tr>`;
+        });
+        
+        table += '</table>';
+        
+        document.getElementById('result').innerHTML = table;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
